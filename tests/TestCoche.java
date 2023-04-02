bfb8145d3cf8e995cc758b7455a0979ec478abd4
carniedom <carniedom@alu.edu.gva.es>
2023-04-02 17:50:28 +0200

    Creado TestCoche
    Creación carpeta tests y la clase TestCoche

---
+++ tests/TestCoche.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author el_mo
+ *
+ */
+class TestCoche {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
